8b312d242e1ffdafaee6d440e53dac93e38a7d58
will.abson <will.abson@a3f5c567-fd0f-3a89-9b71-a290c5a5f590>
2010-10-25 20:31:47 +0000

    Twitter dashlet now supports user lists as well as regular user feeds

--- Twitter Feed Dashlet/config/alfresco/site-webscripts/org/alfresco/components/dashlets/twitter-user-timeline-list.get.js
+++ Twitter Feed Dashlet/config/alfresco/site-webscripts/org/alfresco/components/dashlets/twitter-user-timeline-list.get.js
@@ -1,13 +1,22 @@
 var u = args.twitterUser ? args.twitterUser : "AlfrescoECM";
-var theUrl = "http://twitter.com/status/user_timeline/" + u + ".json?count=10";
 
 function main()
 {
+   if (u.indexOf("/") > 0)
+   {
+      var uparts = u.split("/");
+      var theUrl = "http://api.twitter.com/1/" + uparts[0] + "/lists/" + uparts[1] + "/statuses.json?count=10";
+   }
+   else
+   {
+      var theUrl = "http://twitter.com/status/user_timeline/" + u + ".json?count=10";
+   }
    connector = remote.connect("http");
    result = connector.get(theUrl);
    if (result.status == 200)
    {
       var tweets = eval("(" + result.response + ")");
+      model.username = u;
       model.tweets = tweets;
    }
    else
