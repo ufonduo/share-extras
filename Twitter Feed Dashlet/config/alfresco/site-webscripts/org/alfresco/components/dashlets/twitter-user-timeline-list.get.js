cdcb976dcb1763ac8e6d0bbb09224ab09f5e04d3
will.abson <will.abson@a3f5c567-fd0f-3a89-9b71-a290c5a5f590>
2011-03-11 18:34:52 +0000

    Twitter dashlet reimplemented using client-side rendering with several improvements.   - Added control over tweets page size   - Added Show More button to user feed   - Switched to using v1 API for loading user feed   - Better handling of retweets   - Improved visual display of tweets, adding extra header info   - Display profile images on user feeds

--- Twitter Feed Dashlet/config/alfresco/site-webscripts/org/alfresco/components/dashlets/twitter-user-timeline-list.get.js
+++ Twitter Feed Dashlet/config/alfresco/site-webscripts/org/alfresco/components/dashlets/twitter-user-timeline-list.get.js
@@ -1,34 +1,33 @@
 var u = args.twitterUser ? args.twitterUser : "AlfrescoECM",
       maxId = args.maxId ? args.maxId : null,
-      perPage = 20;
+      perPage = args.pageSize ? args.pageSize : 20;
 
 function main()
 {
    if (u.indexOf("/") > 0)
    {
       var uparts = u.split("/");
-      var theUrl = "http://api.twitter.com/1/" + uparts[0] + "/lists/" + uparts[1] + "/statuses.json";
-      if (maxId != null)
-      {
-         // Request an extra tweet as we already have the first one
-         theUrl += "?per_page=" + (perPage + 1) + "&max_id=" + maxId;
-      }
-      else
-      {
-         theUrl += "?per_page=" + perPage;
-      }
+      var theUrl = "http://api.twitter.com/1/" + uparts[0] + "/lists/" + uparts[1] + 
+         "/statuses.json?per_page=" + stringUtils.urlEncode(perPage);
    }
    else
    {
-      var theUrl = "http://twitter.com/status/user_timeline/" + u + ".json?count=" + perPage;
+      //var theUrl = "http://twitter.com/status/user_timeline/" + u + ".json?count=" + perPage;
+      var theUrl = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + stringUtils.urlEncode(u) + 
+         "&count=" + stringUtils.urlEncode(perPage) + "&include_rts=true";
+   }
+   if (maxId != null)
+   {
+      theUrl += "&max_id=" + maxId;
    }
    connector = remote.connect("http");
    result = connector.get(theUrl);
    if (result.status == 200)
    {
       var tweets = eval("(" + result.response + ")");
+      model.jsonResp = result.response;
       model.username = u;
-      model.tweets = (maxId != null) ? tweets.slice(1) : tweets; // Remove duplicated tweet
+      model.tweets = tweets;
    }
    else
    {
