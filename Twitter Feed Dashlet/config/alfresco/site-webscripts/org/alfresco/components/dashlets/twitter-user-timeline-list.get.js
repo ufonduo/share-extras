5b55279d1f8a73c37331af5585289b7e40033291
will.abson <will.abson@gmail.com>
2011-07-08 20:40:08 +0000

    Twitter Dashlets now able to cache up to 200 new items (100 for search results) rather than 20 previously.

--- Twitter Feed Dashlet/config/alfresco/site-webscripts/org/alfresco/components/dashlets/twitter-user-timeline-list.get.js
+++ Twitter Feed Dashlet/config/alfresco/site-webscripts/org/alfresco/components/dashlets/twitter-user-timeline-list.get.js
@@ -1,7 +1,7 @@
    var u = args.twitterUser ? args.twitterUser : "AlfrescoECM",
       maxId = args.maxId ? args.maxId : null,
       minId = args.minId ? args.minId : null,
-      perPage = args.pageSize ? args.pageSize : 20;
+      perPage = args.pageSize ? args.pageSize : 200;
 
 function main()
 {
@@ -9,21 +9,29 @@ function main()
    {
       var uparts = u.split("/");
       var theUrl = "http://api.twitter.com/1/" + uparts[0] + "/lists/" + uparts[1] + 
-         "/statuses.json?per_page=" + stringUtils.urlEncode(perPage);
+         "/statuses.json?;
+      if (perPage != null)
+      {
+         theUrl += "per_page=" + stringUtils.urlEncode(perPage);
+      }
    }
    else
    {
       //var theUrl = "http://twitter.com/status/user_timeline/" + u + ".json?count=" + perPage;
-      var theUrl = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + stringUtils.urlEncode(u) + 
-         "&count=" + stringUtils.urlEncode(perPage) + "&include_rts=true";
+      var theUrl = "http://api.twitter.com/1/statuses/user_timeline.json?screen_name=" + 
+         stringUtils.urlEncode(u) + "&include_rts=true";
+      if (perPage != null)
+      {
+         theUrl += "&count=" + stringUtils.urlEncode(perPage);
+      }
    }
    if (maxId != null)
    {
-      theUrl += "&max_id=" + maxId;
+      theUrl += "&max_id=" + stringUtils.urlEncode(maxId);
    }
    if (minId != null)
    {
-      theUrl += "&since_id=" + minId;
+      theUrl += "&since_id=" + stringUtils.urlEncode(minId);
    }
    connector = remote.connect("http");
    result = connector.get(theUrl);
