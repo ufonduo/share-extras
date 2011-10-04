0ce24a2870756b2c9f17a5858d619e3e8d4aab8a
florian.maul <florian.maul@a3f5c567-fd0f-3a89-9b71-a290c5a5f590>
2011-10-04 20:01:38 +0000

    Added links to 4.0 javascript docs

--- Sandbox/Javascript Console/source/web/components/console/javascript-console.js
+++ Sandbox/Javascript Console/source/web/components/console/javascript-console.js
@@ -151,6 +151,8 @@ if (typeof Fme == "undefined" || !Fme)
               { text : "W3Schools Javascript Reference", url : "http://www.w3schools.com/jsref/default.asp", target:"_blank"},
               { text : "Alfresco 3.4 Javascript API", url : "http://wiki.alfresco.com/wiki/3.4_JavaScript_API", target:"_blank" },
               { text : "Alfresco 3.4 Javascript Services API", url : "http://wiki.alfresco.com/wiki/3.4_JavaScript_Services_API", target:"_blank" },
+              { text : "Alfresco 4.0 Javascript API", url : "http://wiki.alfresco.com/wiki/4.0_JavaScript_API", target:"_blank" },
+              { text : "Alfresco 4.0 Javascript Services API", url : "http://wiki.alfresco.com/wiki/4.0_JavaScript_Services_API", target:"_blank" },
               { text : "Alfresco Javascript Cookbook", url : "http://wiki.alfresco.com/wiki/JavaScript_API_Cookbook", target:"_blank" },
             ],
             [
