dd48c8b3204a6da62819364a8fb0692f1d93d7a6
loftux.peter@gmail.com <loftux.peter@gmail.com@a3f5c567-fd0f-3a89-9b71-a290c5a5f590>
2012-03-31 12:19:51 +0000

    Updated to align more with Media Preview project, using surf extension to load viewer components. Moved config to to user for viewers to sample directory. Updated pdf.js to revision 0cf4388.

--- trunk/Sandbox/Viewer Extension/source/web/extras/components/preview/pdfjs/pdfviewer.js
+++ trunk/Sandbox/Viewer Extension/source/web/extras/components/preview/pdfjs/pdfviewer.js
@@ -33,6 +33,48 @@ var Cache = function cacheCache(size) {
   };
 };
 
+var ProgressBar = (function ProgressBarClosure() {
+
+  function clamp(v, min, max) {
+    return Math.min(Math.max(v, min), max);
+  }
+
+  function ProgressBar(id, opts) {
+
+    // Fetch the sub-elements for later
+    this.div = document.querySelector(id + ' .progress');
+
+    // Get options, with sensible defaults
+    this.height = opts.height || 100;
+    this.width = opts.width || 100;
+    this.units = opts.units || '%';
+    this.percent = opts.percent || 0;
+
+    // Initialize heights
+    this.div.style.height = this.height + this.units;
+  }
+
+  ProgressBar.prototype = {
+
+    updateBar: function ProgressBar_updateBar() {
+      var progressSize = this.width * this._percent / 100;
+
+      this.div.style.width = progressSize + this.units;
+    },
+
+    get percent() {
+      return this._percent;
+    },
+
+    set percent(val) {
+      this._percent = clamp(val, 0, 100);
+      this.updateBar();
+    }
+  };
+
+  return ProgressBar;
+})();
+
 var RenderingQueue = (function RenderingQueueClosure() {
   function RenderingQueue() {
     this.items = [];
@@ -266,6 +308,10 @@ var PDFView = {
   open: function pdfViewOpen(url, scale) {
     document.title = this.url = url;
 
+    if (!PDFView.loadingBar) {
+      PDFView.loadingBar = new ProgressBar('#loadingBar', {});
+    }
+
     var self = this;
     PDFJS.getPdf(
       {
@@ -408,6 +454,7 @@ var PDFView = {
 //ShareExtras Changes Start
     loadingIndicator.textContent = Alfresco.util.message("label.loading") +' '+ percent + '%';
 //ShareExtras Changes Start
+	PDFView.loadingBar.percent = percent;
   },
 
   load: function pdfViewLoad(data, scale) {
@@ -422,8 +469,8 @@ var PDFView = {
     var errorWrapper = document.getElementById('errorWrapper');
     errorWrapper.setAttribute('hidden', 'true');
 
-    var loadingIndicator = document.getElementById('loading');
-    loadingIndicator.setAttribute('hidden', 'true');
+    var loadingBox = document.getElementById('loadingBox');
+    loadingBox.setAttribute('hidden', 'true');
 
     var sidebar = document.getElementById('sidebarView');
     sidebar.parentNode.scrollTop = 0;
@@ -507,6 +554,24 @@ var PDFView = {
       // Setting the default one.
       this.parseScale(kDefaultScale, true);
     }
+
+    this.metadata = null;
+    var metadata = pdf.catalog.metadata;
+    var info = this.documentInfo = pdf.info;
+    var pdfTitle;
+
+    if (metadata) {
+      this.metadata = metadata = new PDFJS.Metadata(metadata);
+
+      if (metadata.has('dc:title'))
+        pdfTitle = metadata.get('dc:title');
+    }
+
+    if (!pdfTitle && info && info.has('Title'))
+      pdfTitle = info.get('Title');
+
+    if (pdfTitle)
+      document.title = pdfTitle;
   },
 
   setHash: function pdfViewSetHash(hash) {
@@ -1242,10 +1307,6 @@ window.addEventListener('load', function webViewerLoad(evt) {
 
   var sidebarScrollView = document.getElementById('sidebarScrollView');
   sidebarScrollView.addEventListener('scroll', updateThumbViewArea, true);
-}, true);
-
-window.addEventListener('unload', function webViewerUnload(evt) {
-  window.scrollTo(0, 0);
 }, true);
 
 /**
