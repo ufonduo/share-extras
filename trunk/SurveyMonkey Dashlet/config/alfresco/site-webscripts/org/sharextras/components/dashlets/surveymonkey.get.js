0bb398ffc2c868137751f35f8d5e2b78c56a6fda
will.abson@gmail.com <will.abson@gmail.com@a3f5c567-fd0f-3a89-9b71-a290c5a5f590>
2012-09-07 15:28:24 +0000

    4.0 compatibility improvements for SurveyMonkey dashlet

--- trunk/SurveyMonkey Dashlet/config/alfresco/site-webscripts/org/sharextras/components/dashlets/surveymonkey.get.js
+++ trunk/SurveyMonkey Dashlet/config/alfresco/site-webscripts/org/sharextras/components/dashlets/surveymonkey.get.js
@@ -1,5 +1,21 @@
 function main()
 {
+   // Call the repository to see if the user is a site manager or not
+   var userIsSiteManager = false;
+   var obj = context.properties["memberships"];
+   if (!obj)
+   {
+      var json = remote.call("/api/sites/" + page.url.templateArgs.site + "/memberships/" + stringUtils.urlEncode(user.name));
+      if (json.status == 200)
+      {
+         obj = eval('(' + json + ')');
+      }
+   }
+   if (obj)
+   {
+      userIsSiteManager = (obj.role == "SiteManager");
+   }
+   model.userIsSiteManager = userIsSiteManager;
 }
 
 main();
